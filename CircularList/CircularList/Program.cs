aba37a083f8932edcde022c07213925b84d27707
Man-Jamb <78638691+Man-Jamb@users.noreply.github.com>
2021-04-07 16:55:11 +0300

    Add

---
+++ CircularList/CircularList/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Circular
+{
+    public interface ICircularList<T> : IList<T>
+    {
+        void MoveNext();
+        void MoveBack();
+        T Current { get; }
+        T Previous { get; }
+        T Next { get; }
+    }
+
+    public class CircularList<T> : List<T>, ICircularList<T>
+    {
+        private int index = 0;
+
+        public T Current => this[index];
+
+        public T Previous => (index == 0) ? this[Count - 1] : this[index - 1];
+
+        public T Next => (index == (Count - 1)) ? this[0] : this[index + 1];
+
+        public void MoveBack()
+        {
+            if (index == 0)
+            {
+                index = Count - 1;
+            }
+            else
+            {
+                index--;
+            }
+        }
+
+        public void MoveNext()
+        {
+            if (index == (Count - 1))
+            {
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
